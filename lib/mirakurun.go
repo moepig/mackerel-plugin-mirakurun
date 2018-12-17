51e6b7695b819bf31c13c78777e7c1e28d5a73a4
uncho <uncho.tnct@gmail.com>
2018-12-17 14:13:27 +0900

    メモリ使用状況取得実装

--- lib/mirakurun.go
+++ lib/mirakurun.go
@@ -1,8 +1,11 @@
 package mpmirakurun
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	mp "github.com/mackerelio/go-mackerel-plugin"
 )
@@ -13,11 +16,57 @@ type MirakurunPlugin struct {
 	Tempfile string
 }
 
-var graphdef = map[string]mp.Graphs{}
+type Status struct {
+	Process struct {
+		MemoryUsage struct {
+			Rss       int `json:"rss"`
+			HeapTotal int `json:"heapTotal"`
+			HeapUsed  int `json:"heapUsed"`
+			External  int `json:"external"`
+		} `json:"memoryUsage"`
+	} `json:"process"`
+}
+
+var graphdef = map[string]mp.Graphs{
+	"memoryUsage": mp.Graphs{
+		Label: "Mirakurun Memory Usage",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "rss", Label: "RSS", Diff: false},
+			{Name: "heapTotal", Label: "Heap Total", Diff: false},
+			{Name: "heapUsed", Label: "Heap Used", Diff: false},
+		},
+	},
+}
 
 // FetchMetrics interface for mackerelplugin
 func (m MirakurunPlugin) FetchMetrics() (map[string]float64, error) {
+	// call status api
+	url := fmt.Sprintf("http://%s/api/status", m.Target)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	byteArray, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var status Status
+	err = json.Unmarshal(byteArray, &status)
+	if err != nil {
+		return nil, err
+	}
+
+	// mapping to metrics
 	stat := make(map[string]float64)
+
+	stat["rss"] = float64(status.Process.MemoryUsage.Rss)
+	stat["heapTotal"] = float64(status.Process.MemoryUsage.HeapTotal)
+	stat["heapUsed"] = float64(status.Process.MemoryUsage.HeapUsed)
 
 	return stat, nil
 }
